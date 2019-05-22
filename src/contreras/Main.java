6bfae7049b723b7fa181775a782f05de1df7aa84
hibroseph <iwillridgley@gmail.com>
2019-05-22 17:30:21 -0600

    changed welcome to bob

--- src/contreras/Main.java
+++ src/contreras/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Beto.");
-        System.out.println("Hello, my name is now Joe");
+        System.out.println("Hello, my name is now Bob");
     }
 }
